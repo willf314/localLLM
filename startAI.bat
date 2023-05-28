be088f73dcb2199b4c42acec0592e5211c9a85f5
willf314 <111627331+willf314@users.noreply.github.com>
2023-05-28 22:21:08 +1000

    removed --reload from uvicorn startup

--- startAI.bat
+++ startAI.bat
@@ -4,6 +4,6 @@ set LLM_QUERY_ASYNC_URL=http://localhost:8001/query-async
 set LLM_EMBEDDING_URL=http://localhost:8000/get-embedding
 set QDRANT_URL=http://localhost:6333
 set EMBEDDING_MODEL_PATH=C:\data\Projects\test\instructor-xl
-start "" uvicorn llm-embedding:app --port 8000 --reload
-start "" uvicorn llm-query:app --port 8001 --reload
-start "" uvicorn control:app --port 8002 --reload
+start "" uvicorn llm-embedding:app --port 8000
+start "" uvicorn llm-query:app --port 8001
+start "" uvicorn control:app --port 8002
